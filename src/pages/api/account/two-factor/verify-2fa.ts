91a236a370a7e364fc4b00b4225c2e3c937cddcc
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-09-20 19:03:38 -0300

    router verify 2fa

--- src/pages/api/account/two-factor/verify-2fa.ts
+++ src/pages/api/account/two-factor/verify-2fa.ts
@@ -1,38 +1,50 @@
+import { accounts } from '@prisma/client';
+import { withSessionRoute } from '../../../../lib/session';
 import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from 'src/database/instance';
-
+import prisma from '../../../../prisma';
 import speakeasy from 'speakeasy';
 
-export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { token } = req.body;
-    const user = req.session.user;
+const post = withSessionRoute(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      const { token } = req.body;
+      const user = req.session.user as accounts;
 
-    if (!user) {
-      return res.status(403).json({ message: 'Not authorized.' });
-    }
+      if (!user) {
+        return res.status(403).json({ message: 'Not authorized.' });
+      }
 
-    const verified = speakeasy.totp.verify({
-      secret: user.twoFASecret,
-      encoding: 'base32',
-      token,
-    });
-
-    if (verified) {
-      await prisma.accounts.update({
-        where: { id: user.id },
-        data: { twoFAEnabled: true },
+      const verified = speakeasy.totp.verify({
+        secret: String(user.twoFASecret),
+        encoding: 'base32',
+        token,
       });
-      return res.json({ success: true });
-    } else {
-      return res.json({ success: false });
+
+      if (verified) {
+        await prisma.accounts.update({
+          where: { id: user.id },
+          data: { twoFAEnabled: true },
+        });
+        return res.json({ success: true });
+      } else {
+        return res.json({ success: false });
+      }
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ success: false, error: 'Internal server error' });
+    } finally {
+      await prisma.$disconnect();
     }
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(500)
-      .json({ success: false, error: 'Internal server error' });
-  } finally {
-    await prisma.$disconnect();
+  },
+);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  switch (req.method) {
+    case 'POST':
+      return post(req, res);
+    default:
+      return res.status(405).json({ message: 'Method not allowed' });
   }
 };
