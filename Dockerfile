33aeba352eae924996e952311b91799e63ce489f
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-09-21 19:30:22 -0300

    chore: update docker & scripts

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,17 @@ FROM node
 WORKDIR /usr/app/
 COPY package.json ./
 RUN npm install
-COPY . .
-EXPOSE 3001
 
-CMD ["npm", "run", "dev"]
+# Copiar todo o conteúdo do diretório do projeto para o container
+COPY . .
+
+# Inspeção manual para verificar os conteúdos do diretório src/prisma/
+RUN ls -l /usr/app/src/prisma/
+
+# Execute a geração do Prisma
+RUN npx prisma generate
+
+EXPOSE 3000
+
+# Execute as migrações, seeds e então a aplicação
+CMD ["sh", "-c", "npm run migrations:run && npm run create:seeds && npm run dev"]
--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,15 +1,48 @@
 import prisma from '.';
-import fs from 'fs/promises';
-import path from 'path';
 
 async function main() {
-  const seedsPath = path.join(__dirname, '/seeds');
+  let createdAccount = await prisma.accounts.findUnique({
+    where: { name: 'nekiro' },
+  });
 
-  for (const file of await fs.readdir(seedsPath)) {
-    const seed = require(path.join(seedsPath, file)).default;
-    // @ts-ignore
-    await prisma[seed.table].createMany({
-      data: seed.data,
+  if (!createdAccount) {
+    createdAccount = await prisma.accounts.create({
+      data: {
+        name: 'nekiro',
+        password: '1448f93979c89c4e6e8664df211b40ace882f6d5',
+        email: 'nekirodev@dev.com',
+        twoFAEnabled: false,
+      },
+    });
+  }
+
+  let createdPlayer = await prisma.players.findUnique({
+    where: { name: 'Nekiro' },
+  });
+
+  if (!createdPlayer) {
+    createdPlayer = await prisma.players.create({
+      data: {
+        name: 'Nekiro',
+        account_id: createdAccount.id,
+        vocation: 0,
+        sex: 1,
+      },
+    });
+  }
+
+  const existingNews = await prisma.aac_news.findFirst({
+    where: { title: 'Welcome' },
+  });
+
+  if (!existingNews) {
+    await prisma.aac_news.create({
+      data: {
+        title: 'Welcome',
+        content: 'Welcome to shibaac...',
+        imageUrl: '',
+        authorId: createdPlayer.id,
+      },
     });
   }
 }
