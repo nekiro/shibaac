4da91f133e5d5c2c9e277d1ec7a0be24016e0400
Pedro Giampietro <pedrogiampietro@hotmail.com>
2023-09-26 16:20:52 -0300

    add: mocks deaths player

---
+++ src/components/Timeline.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Box, VStack, Circle, Text, Flex, Icon } from '@chakra-ui/react';
+import { BiGhost, BiTargetLock } from 'react-icons/bi';
+import { differenceInDays, formatDistanceToNow } from 'date-fns';
+
+export function timestampToDaysAgo(timestamp) {
+  const date = new Date(timestamp * 1000);
+  const daysDifference = differenceInDays(new Date(), date);
+
+  if (daysDifference <= 1) {
+    return formatDistanceToNow(date) + ' ago';
+  } else {
+    return daysDifference + ' days ago';
+  }
+}
+
+export function Timeline({ items }) {
+  return (
+    <Box
+      position="relative"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <VStack align="start" spacing={10} w="100%" position="relative">
+        <Box
+          position="absolute"
+          bg="purple.300"
+          width="2px"
+          height="calc(100% - 20px)"
+          left="calc(50% - 1px)"
+          top="10px"
+          zIndex="1"
+        />
+        {items.map((item, index) => (
+          <TimelineItem
+            key={index}
+            date={item.date}
+            text={item.text}
+            deathType={item.killedByPlayer}
+          />
+        ))}
+      </VStack>
+    </Box>
+  );
+}
+
+function TimelineItem({ date, text, deathType }) {
+  let formattedDate = timestampToDaysAgo(date);
+
+  let DeathIcon;
+  if (deathType) {
+    DeathIcon = BiTargetLock;
+  } else {
+    DeathIcon = BiGhost;
+  }
+
+  return (
+    <Flex w="100%" alignItems="center" justifyContent="space-between">
+      <Text flex="1" textAlign="right" pr={2} fontWeight="bold">
+        {formattedDate}
+      </Text>
+      <Box position="relative" zIndex="1">
+        <Circle
+          size="34px"
+          bg="purple.300"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Icon as={DeathIcon} color="white" fontSize={20} />
+        </Circle>
+      </Box>
+      <Text flex="1" pl={2}>
+        {text}
+      </Text>
+    </Flex>
+  );
+}
+
+export default Timeline;
--- src/pages/character/[name].jsx
+++ src/pages/character/[name].jsx
@@ -1,15 +1,64 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import Panel from 'src/components/Panel';
-import { fetchApi } from 'src/lib/request';
+import Panel from '../../components/Panel';
+import { fetchApi } from '../../lib/request';
 import {
   timestampToDate,
   vocationIdToName,
   groupToName,
   secondsToTime,
-} from 'src/lib';
+} from '../../lib';
 import { useRouter } from 'next/router';
-import StrippedTable from 'src/components/StrippedTable';
-import Label from 'src/components/Label';
+import StrippedTable from '../../components/StrippedTable';
+import Label from '../../components/Label';
+import { Timeline } from '../../components/Timeline';
+import { Box, VStack, HStack, Circle, Text, Flex } from '@chakra-ui/react';
+
+const player_deaths = [
+  {
+    player_id: 1,
+    time: 1694755956,
+    level: 5,
+    killed_by: 'Orc Warrior',
+    is_player: false,
+    mostdamage_by: 'Dragon',
+    mostdamage_is_player: false,
+    unjustified: true,
+    mostdamage_unjustified: true,
+  },
+  {
+    player_id: 1,
+    time: 1695155956,
+    level: 10,
+    killed_by: 'Player123',
+    is_player: true,
+    mostdamage_by: 'Player456',
+    mostdamage_is_player: true,
+    unjustified: false,
+    mostdamage_unjustified: false,
+  },
+  {
+    player_id: 1,
+    time: 1693755956,
+    level: 10,
+    killed_by: 'Player123',
+    is_player: true,
+    mostdamage_by: 'Player456',
+    mostdamage_is_player: true,
+    unjustified: false,
+    mostdamage_unjustified: false,
+  },
+  {
+    player_id: 1,
+    time: 1695745956,
+    level: 10,
+    killed_by: 'Player123',
+    is_player: true,
+    mostdamage_by: 'Player456',
+    mostdamage_is_player: true,
+    unjustified: false,
+    mostdamage_unjustified: false,
+  },
+];
 
 export default function Character() {
   const router = useRouter();
@@ -139,16 +188,18 @@ export default function Character() {
         />
       </Panel>
 
-      {state.player.player_deaths.length > 0 && (
-        <Panel header="Deaths">
-          <StrippedTable
-            head={[{ text: 'Date' }, { text: 'Message' }]}
-            body={state.player.playerDeaths.map((death) => [
-              { text: timestampToDate(death.time) },
-              { text: `Died at level ${death.level} by ${death.killed_by}` },
-            ])}
-          />
-        </Panel>
+      {player_deaths.length > 0 && (
+        <Box>
+          <Panel header="Deaths">
+            <Timeline
+              items={player_deaths.map((death) => ({
+                date: death.time,
+                text: `Died at level ${death.level} by ${death.killed_by}`,
+                killedByPlayer: death.is_player,
+              }))}
+            />
+          </Panel>
+        </Box>
       )}
 
       {/* <span className="label label-danger">Deleted</span>  */}
