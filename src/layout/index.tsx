9d4b9354c0a162425fd57215959a118486dfe566
Marcin Jałocha <nekiro.dev@gmail.com>
2024-11-26 12:37:04 +0100

    Use pixel based width for layout image

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -9,7 +9,7 @@ const Layout = ({ children }: PropsWithChildren) => {
 	return (
 		<Box w={{ base: "95%", md: "70%" }} marginX={"auto"} marginY={{ base: "1em", md: 0 }}>
 			<Head />
-			<Image width="15%" marginLeft="auto" marginRight="auto" marginBottom="15px" marginTop="15px" src="/images/header.png" alt="shibaac" />
+			<Image width="230px" marginLeft="auto" marginRight="auto" marginBottom="15px" marginTop="15px" src="/images/header.png" alt="shibaac" />
 			<NavBar />
 			<Flex flexDirection={{ base: "column", md: "row" }}>
 				<Box flexGrow="1" marginRight={{ base: 0, md: "3em" }} order={{ base: 2, md: 1 }}>
