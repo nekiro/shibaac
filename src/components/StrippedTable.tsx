6b8d233b8ac027ce83aff7aa2130b2e024d25f6d
Marcin Jałocha <nekiro.dev@gmail.com>
2024-11-26 11:02:06 +0100

    Add loader to striped table

--- src/components/StrippedTable.tsx
+++ src/components/StrippedTable.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Link from "./Link";
-import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td, Text } from "@chakra-ui/react";
+import Loader from "./Loader";
 
 export interface StripedTableProps {
 	head?: { text: string }[];
 	body: { text: string | number; href?: string }[][];
+	isLoading?: boolean;
 }
 
-const StripedTable = ({ head, body }: StripedTableProps) => {
+const StripedTable = ({ head, body, isLoading }: StripedTableProps) => {
+	if (isLoading) {
+		return <Loader />;
+	}
+
 	return (
 		<Table variant="simple">
 			{head && (
@@ -19,14 +25,23 @@ const StripedTable = ({ head, body }: StripedTableProps) => {
 					</Tr>
 				</Thead>
 			)}
+
 			<Tbody>
-				{body.map((row, index) => (
-					<Tr key={`${index}`}>
-						{row.map((data, dataIndex) => (
-							<Td key={`${data.text} ${dataIndex}`}>{data.href ? <Link href={data.href} text={String(data.text)} /> : data.text}</Td>
-						))}
+				{body.length === 0 ? (
+					<Tr>
+						<Td>
+							<Text>No data.</Text>
+						</Td>
 					</Tr>
-				))}
+				) : (
+					body.map((row, index) => (
+						<Tr key={`${index}`}>
+							{row.map((data, dataIndex) => (
+								<Td key={`${data.text} ${dataIndex}`}>{data.href ? <Link href={data.href} text={String(data.text)} /> : data.text}</Td>
+							))}
+						</Tr>
+					))
+				)}
 			</Tbody>
 		</Table>
 	);
