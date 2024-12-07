a92b8de424024ef605b2add445bc642c0cd8e6f5
Marcin Jałocha <nekiro.dev@gmail.com>
2024-12-07 16:52:14 +0100

    Remove redundant files

--- src/schemas/ChangeEmail.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const changeEmailSchema = yup.object().shape({
-	newEmail: yup.string().email("Must be a valid email.").required("Required"),
-	password: yup.string().required("Required"),
-});
-
-export type ChangeEmail = yup.InferType<typeof changeEmailSchema>;
