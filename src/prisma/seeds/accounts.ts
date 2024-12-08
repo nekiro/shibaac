46e34db6c9fe1cff4e6ee5dd21fa5aafe4ba42a8
Marcin Jałocha <nekiro.dev@gmail.com>
2024-12-08 11:25:09 +0100

    Add seeds

--- src/prisma/seeds/accounts.ts
+++ src/prisma/seeds/accounts.ts
@@ -13,6 +13,12 @@ const seed = async () => {
 				email: "nekiro@nekiro.dev",
 				type: 5,
 			},
+			{
+				name: "admin",
+				password: "d033e22ae348aeb5660fc2140aec35850c4da997", // admin
+				email: "admin@admin.dev",
+				type: 5,
+			},
 		],
 	});
 };
--- src/prisma/seeds/news.ts
+++ src/prisma/seeds/news.ts
@@ -12,6 +12,16 @@ const seed = async () => {
 				authorId: null,
 				content: "Welcome to shibaac, this is a live preview of current state of the AAC.",
 			},
+			{
+				title: "Second News",
+				authorId: null,
+				content: "This is the second news for testing",
+			},
+			{
+				title: "News 3",
+				authorId: null,
+				content: "xD",
+			},
 		],
 	});
 };
